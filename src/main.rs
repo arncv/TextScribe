b5bf35f722187625cd6d9f3792e8708f82f47975
Arnav <arnavgoel7903@gmail.com>
2023-08-28 02:43:14 +0530

    lil changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use std::fs;
-use std::io::{self, Read};
 use pulldown_cmark::{html, Options, Parser};
 use clap::{Arg, App};
 
 fn convert_markdown_to_html(input: &str) -> String {
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_TABLES);
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-
+    let options = Options::ENABLE_TABLES | Options::ENABLE_STRIKETHROUGH;
     let parser = Parser::new_ext(input, options);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
@@ -15,14 +11,24 @@ fn convert_markdown_to_html(input: &str) -> String {
 }
 
 fn convert_file_to_html(file_path: &str) -> Result<String, std::io::Error> {
-    let mut file = fs::File::open(file_path)?;
-    let mut markdown = String::new();
-    file.read_to_string(&mut markdown)?;
+    let markdown = fs::read_to_string(file_path)?;
     Ok(convert_markdown_to_html(&markdown))
 }
 
 fn save_html_to_file(html: &str, output_file_path: &str) -> Result<(), std::io::Error> {
     fs::write(output_file_path, html)
+}
+
+fn get_theme_css(theme: &str) -> &str {
+    match theme {
+        "dark" => "<style>body { background-color: black; color: white; }</style>",
+        "light" => "<style>body { background-color: white; color: black; }</style>",
+        "default" => "",
+        _ => {
+            eprintln!("Warning: Unrecognized theme '{}'. Using default theme.", theme);
+            ""
+        }
+    }
 }
 
 fn main() {
@@ -44,23 +50,19 @@ fn main() {
              .default_value("default"))
         .get_matches();
 
-    let input_file_path = matches.value_of("input").unwrap();
-    let output_file_path = matches.value_of("output").unwrap();
-    let theme = matches.value_of("theme").unwrap();
+    let input_file_path = matches.value_of("input").expect("Failed to get input file path");
+    let output_file_path = matches.value_of("output").expect("Failed to get output file path");
+    let theme = matches.value_of("theme").expect("Failed to get theme");
 
-    // Basic theming (for demonstration purposes)
-    let css = match theme {
-        "dark" => "<style>body { background-color: black; color: white; }</style>",
-        "light" => "<style>body { background-color: white; color: black; }</style>",
-        _ => "", // default theme
-    };
+    let css = get_theme_css(theme);
 
     match convert_file_to_html(input_file_path) {
         Ok(mut html) => {
             html.insert_str(0, css); // Prepend the CSS to the HTML output
-            match save_html_to_file(&html, output_file_path) {
-                Ok(_) => println!("Conversion successful. HTML saved to {}", output_file_path),
-                Err(err) => eprintln!("Error saving HTML to file: {}", err),
+            if let Err(err) = save_html_to_file(&html, output_file_path) {
+                eprintln!("Error saving HTML to file: {}", err);
+            } else {
+                println!("Conversion successful. HTML saved to {}", output_file_path);
             }
         },
         Err(err) => eprintln!("Error converting file to HTML: {}", err),
